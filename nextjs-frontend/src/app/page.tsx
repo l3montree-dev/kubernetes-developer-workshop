6001f1118c6177e622437c8c4553585dc370debf
Tim Bastin <bastin.tim@gmail.com>
2025-02-19 15:06:50 +0100

    adds docker compose, adds dockerfile for frontend

--- nextjs-frontend/src/app/page.tsx
+++ nextjs-frontend/src/app/page.tsx
@@ -1,5 +1,17 @@
+import { connection } from "next/server";
 
 export default async function Home() {
+  await connection()
+  if (!process.env.COLOR_API_URL) {
+      return (
+          <div className="flex flex-row items-center flex-1 h-screen justify-center">
+              <h2 className="text-4xl font-bold text-center">
+                Ich kann keine COLOR_API_URL Umgebungsvariable finden!
+              </h2>
+          </div>
+        );
+  }
+
   const { hex } = await fetch(process.env.COLOR_API_URL + "/color").then((res) => res.json());
     
   console.log(hex)
